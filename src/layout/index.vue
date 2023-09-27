92a857870d5e849bcccc6ab8bea7728afd4770f3
Ivanna Cantelberg <ivannacantelberg@gmail.com>
2023-09-27 20:43:18 +0200

    Fixed main layout

--- src/layout/index.vue
+++ src/layout/index.vue
@@ -1,24 +1,46 @@
 <template>
   <a-layout>
-    <a-layout-sider v-model:collapsed="collapsed" :trigger="null" collapsible>
-      <SideNav :collapsed="collapsed" />
-    </a-layout-sider>
-    <a-layout-header style="background: #fff">
-      <menu-unfold-outlined
-        v-if="collapsed"
-        class="trigger"
-        @click="toggleCollapsed" />
-      <menu-fold-outlined v-else class="trigger" @click="toggleCollapsed" />
-    </a-layout-header>
-    <a-layout-content
+    <a-layout-sider
+      :trigger="null"
       :style="{
-        margin: '24px 16px',
-        padding: '24px',
-        background: '#fff',
-        height: '100%',
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        paddingTop: '64px',
+        left: 0,
+        top: 0,
+        bottom: 0,
       }">
-      <router-view />
-    </a-layout-content>
+      <SideNav />
+    </a-layout-sider>
+    <a-layout :style="{ marginLeft: '200px' }">
+      <a-layout-header
+        :style="{
+          position: 'fixed',
+          zIndex: 1,
+          width: '100%',
+          backgroundColor: '#fff',
+        }">
+      </a-layout-header>
+      <a-layout-content
+        :style="{
+          margin: '64px 0 0',
+          overflow: 'auto',
+          padding: '24px 24px 0',
+        }">
+        <router-view />
+
+        <a-layout-footer :style="{ textAlign: 'center' }">
+          Vue.js 3 + TypeScript + Ant Design Vue @2021 Created by
+          <a
+            href="https://t.me/IvannaCantelberg"
+            target="_blank"
+            rel="noopener noreferrer">
+            @IvannaCantelberg
+          </a>
+        </a-layout-footer>
+      </a-layout-content>
+    </a-layout>
   </a-layout>
 </template>
 
